ff75275ba26af18af5efc9b16793eea79a2b8ef3
Nickolay Popov <nickolaypopov@gmail.com>
2021-04-19 16:18:17 +0500

    lesson1

---
+++ src/lesson1/HomeWorkApp.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class homework {
+
+    public static void main(String[] args) {
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+}
