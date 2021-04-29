e31e87980b2f3ee0b471c596dca981026b46c82b
Nickolay <nickolaypopov@gmail.com>
2021-04-29 09:57:07 +0500

    homework4
    step 1 (without AI)

---
+++ src/lesson4/HomeWorkApp4.java
@@ -0,0 +1,168 @@
+package lesson4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWorkApp4 {
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+
+    public static char[][] map;
+    public static int field_length = 3;// + random.nextInt(3);
+    public static int mapSizeX = field_length;
+    public static int mapSizeY = field_length;
+    public static int winLength = 3;
+
+    public static char human = 'X';
+    public static char ai = 'O';
+    public static char empty_field = '_';
+
+    public static void createMap() {
+
+        map = new char[mapSizeY][mapSizeX];
+
+        for (int y = 0; y < mapSizeY; y++) {
+            for (int x = 0; x < mapSizeX; x++) {
+                map[y][x] = empty_field;
+            }
+        }
+
+    }
+
+    public static void showMap() {
+
+        for (int y = 0; y < mapSizeY; y++) {
+            for (int x = 0; x < mapSizeX; x++) {
+                System.out.print(map[y][x] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("----------");
+
+    }
+
+    public static boolean isDraw() {
+
+        for (int y = 0; y < mapSizeY; y++) {
+            for (int x = 0; x < mapSizeX; x++) {
+                if (map[y][x] == empty_field) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    public static boolean isValidCell(int x, int y) {
+        return x >= 0 && x < mapSizeX && y >= 0 && y < mapSizeY;
+    }
+
+    public static boolean isEmptyCell(int x, int y) {
+        return map[y][x] == empty_field;
+    }
+
+    public static void humanTurn() {
+        int x, y;
+
+        do {
+            System.out.println("Enter your turn coordinates from 1 before " + mapSizeX + ":");
+            y = scanner.nextInt() - 1;
+            x = scanner.nextInt() - 1;
+        } while (!isValidCell(x, y) || !isEmptyCell(x, y));
+        map[y][x] = human;
+
+    }
+
+    public static void aiTurn() {
+        int x, y;
+
+        do {
+            y = random.nextInt(mapSizeY);
+            x = random.nextInt(mapSizeX); //[0;mapSize)
+        } while (!isEmptyCell(x, y));
+
+        /**
+         *
+         */
+
+        System.out.println("AI turn is [" + (y + 1) + ":" + (x + 1) + "]");
+        map[y][x] = ai;
+
+    }
+
+    public static boolean isWin(char player) {
+        int count_player;
+
+        for (int i = 0; i <= (map.length - winLength); i++){
+            for (int j = 0; j <= (map.length - winLength); j++) {
+                for (int y = 0; y < winLength; y++) {
+                    count_player = 0;
+                    for (int x = 0; x < winLength; x++) {
+                        if (map[i + y][j + x] == player) count_player++;
+                    }
+                    if (count_player == winLength) return true;
+                }
+                for (int x = 0; x < winLength; x++) {
+                    count_player = 0;
+                    for (int y = 0; y < winLength; y++) {
+                        if (map[i + y][j + x] == player) count_player++;
+                    }
+                    if (count_player == winLength) return true;
+                }
+                count_player = 0;
+                for (int k = 0; k < winLength; k++) {
+                    if (map[i + k][j + k] == player) count_player++;
+                }
+                if (count_player == winLength) return true;
+                count_player = 0;
+                for (int k = 0; k < winLength; k++) {
+                    if (map[i + k][j - k + winLength - 1] == player) count_player++;
+                }
+                if (count_player == winLength) return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+
+        if (field_length == 5) winLength++;
+        createMap();
+        showMap();
+
+        while (true) {
+            humanTurn();
+            showMap();
+
+            if (isWin(human)) {
+                System.out.println("Human win!!!");
+                break;
+            }
+
+            if (isDraw()) {
+                System.out.println("Is Draw!!!");
+                break;
+            }
+
+            aiTurn();
+            showMap();
+
+            if (isWin(ai)) {
+                System.out.println("Human win!!!");
+                break;
+            }
+
+            if (isDraw()) {
+                System.out.println("Is Draw!!!");
+                break;
+            }
+
+        }
+        System.out.println("Game over!");
+
+    }
+
+}
