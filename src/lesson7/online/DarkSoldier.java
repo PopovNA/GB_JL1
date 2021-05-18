3186c026866afad53ed586df59684fb21e7e3a26
Nickolay Popov <nickolaypopov@gmail.com>
2021-05-18 18:00:13 +0500

    lesson 7

---
+++ src/lesson7/online/DarkSoldier.java
@@ -0,0 +1,15 @@
+package lesson7.online;
+
+import lesson7.online.models.Soldier;
+
+/**
+ * Created by Aleksandr Gladkov [Anticisco]
+ * Date: 12.05.2021
+ */
+
+public class DarkSoldier extends Soldier {
+
+    public DarkSoldier(String name, int attack, float defense, float hp) {
+        super(name, attack, defense, hp);
+    }
+}
